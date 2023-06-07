ca057a4c3330fb6fe793422965af6f99c01bc41e
idris <muhmmadidris15@gmail.com>
2023-06-07 09:35:05 +0700

    add sidebar

---
+++ js/handler.js
@@ -0,0 +1,25 @@
+import { menu } from "menu.js";
+
+function searchListener() {}
+function openNavListener() {
+  $("#open").classList.toggle("open");
+  alert("heloo");
+}
+
+export { searchListener, openNavListener };
+
+  /* <div class="home-card d-flex">
+<div class="home-card-content col p-3">
+    <div class="p-3 text-center">
+        <span class="price">$45.95</span>
+        <h5 class="h5-homeCard">Grilled Fillet</h5>
+        <p class="">Pork fillet, ginger, garlic, honey,
+            pepper & canola oil.creamy
+            chesapeake crab dip with
+            artichoke, baked and topped with
+            cheddar cheese, with crusty bread.</p>
+    </div>
+</div>
+<img src="../img/image 8.png" class="home-card-img">
+</div> */
+
--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
-const handler = require('handler.js');
+// import { searchListener, openNavListener } from "./handler.js";
 
 $(document).ready(function () {
-    
+  //   $("#search").click(searchListener);
+  $("#nav-menu").click(function () {
+    alert("a");
+  });
+  alert("heloo");
 });
+
+// <ul id="account">
+// <li><a href="#">userName</a></li>
+// <li><a href="#">Checkout</a></li>
+// <li><a href="#">my Service</a></li>
+// </ul>
+// </li>
